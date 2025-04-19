eb9f5421cf1d5781087e9f91d933f7caae82fda4
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-04-18 20:30:59 -0500

    test: add a new test to check that the server is launched successfully

--- apt_app/tests.py
+++ apt_app/tests.py
@@ -1,3 +1,13 @@
 def test_dry_run():
-    """A super simple test to verify our workflow runs."""
+    """A super simple test to verify the GitHub Actions workflow runs."""
     assert True
+
+
+def test_server_available():
+    """Test that the root URL ('/') responds with HTTP 200."""
+    from django.test import Client
+
+    client = Client()
+    response = client.get("/")
+
+    assert response.status_code == 200
