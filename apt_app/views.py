e8cb4f76ae64c6bf05c750a157e00eed9b776a4a
mmbarros-git <mmbarros@uchicago.edu>
2025-04-25 13:49:26 -0500

    Add home view using render() function

--- apt_app/views.py
+++ apt_app/views.py
@@ -1,8 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Server is running", status=200)
+    return render(request, 'home.html')
+
