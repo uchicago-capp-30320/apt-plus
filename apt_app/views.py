f394926ae6afc7062dab67c14d02cbb6d45a3f4b
mmbarros-git <mmbarros@uchicago.edu>
2025-04-26 14:34:12 -0500

    Comment unused HttpResponse import to fix lint error

--- apt_app/views.py
+++ apt_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse("Server is running", status=200)
 
 def home(request):
     return render(request, 'home.html')
