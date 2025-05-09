d0413e490a3fb8e987d13e6e8d62843afd624a5f
Miguel Perez <miguelperez@uchicago.edu>
2025-05-09 00:59:15 -0500

    fix: deleted rest_framework on views.py

--- apt_app/views.py
+++ apt_app/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 from apt_app.views_functions.function_fetch_all_data import fetching_all_data
 
 # Create your views here.
@@ -10,8 +10,8 @@ def home(request):
     return HttpResponse("Server is running", status=200)
 
 
-@api_view(["POST"])
+@csrf_exempt
 def fetch_all_data(request):
-    address = request.data.get("address")
+    address = request.POST.get("address")
     json_response = fetching_all_data(address)
     return json_response
