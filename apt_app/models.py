51c84a1145da0047d9ea9f53124968fe5c79ba00
fuyukitn <ftani@uchicago.edu>
2025-05-14 22:25:21 -0500

    feature(models): update TransitRoute to relace id with route_id

--- apt_app/models.py
+++ apt_app/models.py
@@ -216,7 +216,7 @@ class TransitStop(LocationBasedFacilities):
 
 
 class TransitRoute(models.Model):
-    id = models.AutoField(primary_key=True)
+    route_id = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=50, choices=TransitType.choices, default=TransitType.OTHER)
     geometry = gis_models.MultiLineStringField()
