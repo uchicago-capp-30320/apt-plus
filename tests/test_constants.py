318b2e80bac5a284a3b54866bf945f88c504632b
fuyukitn <ftani@uchicago.edu>
2025-05-16 19:11:28 -0500

    feat(constants): add constants module and constants.json

---
+++ tests/test_constants.py
@@ -0,0 +1,13 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from config.constants import WALKING_SPEED_PER_MIN, HP_BOUNDS
+
+def test_constants():
+    assert isinstance(HP_BOUNDS, list)
+    assert isinstance(WALKING_SPEED_PER_MIN, int)
+    print("Works as expected.")
+
+
+if __name__ == "__main__":
+    test_constants()
