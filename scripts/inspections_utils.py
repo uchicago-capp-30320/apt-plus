73c927f27350fa67e5039f3f5169dfecaadb1047
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-05-26 16:35:48 -0500

    feat(insp): enhance raw violations ingestion with bulk_create option

--- scripts/inspections_utils.py
+++ scripts/inspections_utils.py
@@ -7,6 +7,9 @@ import duckdb
 from dotenv import load_dotenv
 import os
 import json
+from datetime import datetime
+from django.contrib.gis.geos import Point
+from apt_app.models import Violation
 
 load_dotenv()
 
@@ -355,6 +358,62 @@ def generate_summary_for_one_address(address, df, model):
         return None
     # if df_filtered is not empty, summarize
     summary_json = llm_summarize_for_one_address(address=address, df=df, model=model)
-    return summary_json
     print(f"SUMMARY: {summary_json}")
     print("\n----------------------------")
+    return summary_json
+
+
+# ----- DATA INGESTION -----
+
+
+def convert_date(
+    date_str: str, input_format: str = "%m/%d/%Y", output_format: str = "%Y-%m-%d"
+) -> str | None:
+    """
+    Convert a date string from one format to another.
+
+    :param date_str: The date string to convert.
+    :param input_format: The format of the input date string.
+    :param output_format: The desired format for the output date string.
+    :return: The converted date string.
+    """
+    if isinstance(date_str, float):
+        return None
+    # Check for empty or whitespace-only strings
+    if not date_str.strip():
+        return None
+    try:
+        return datetime.strptime(date_str, input_format).strftime(output_format)
+    except ValueError as e:
+        raise ValueError(f"Error converting date: {e}")
+
+
+def create_one_violation_object(row) -> Violation:
+    return Violation(
+        violation_id=row["ID"],
+        violation_last_modified_date=convert_date(row["VIOLATION LAST MODIFIED DATE"]),
+        violation_date=convert_date(row["VIOLATION DATE"]),
+        violation_code=row["VIOLATION CODE"],
+        violation_status=row["VIOLATION STATUS"],
+        violation_status_date=convert_date(row["VIOLATION STATUS DATE"]),
+        violation_description=row["VIOLATION DESCRIPTION"],
+        violation_location=row["VIOLATION LOCATION"],
+        violation_inspector_comments=row["VIOLATION INSPECTOR COMMENTS"],
+        violation_ordinance=row["VIOLATION ORDINANCE"],
+        inspector_id=row["INSPECTOR ID"],
+        inspection_number=row["INSPECTION NUMBER"],
+        inspection_status=row["INSPECTION STATUS"],
+        inspection_waived=row["INSPECTION WAIVED"],
+        inspection_category=row["INSPECTION CATEGORY"],
+        department_bureau=row["DEPARTMENT BUREAU"],
+        address=row["ADDRESS"],
+        street_number=row["STREET NUMBER"],
+        street_direction=row["STREET DIRECTION"],
+        street_name=row["STREET NAME"],
+        street_type=row["STREET TYPE"],
+        property_group=row["PROPERTY GROUP"],
+        ssa=row["SSA"],
+        latitude=row["LATITUDE"],
+        longitude=row["LONGITUDE"],
+        location=Point(float(row["LATITUDE"]), float(row["LONGITUDE"])),  # FIXME
+    )
