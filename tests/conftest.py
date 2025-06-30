6369cd8ea3228340bccd76527a4116a85c092cdd
Michael Rosenbaum <michaelr526@gmail.com>
2025-06-30 16:13:35 -0400

    fix: revert ruff change

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 import os
 import pytest
-from django.conf import settings
 
 
 # Chat-GPT suggestion to avoid whitenoise static files, as playwright chromium
 # browser can't access them for some reason
 @pytest.fixture(autouse=True)
-def override_staticfiles_storage(django_settings):
-    django_settings.STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-    django_settings.DEBUG = True
+def override_staticfiles_storage(settings):
+    settings.STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+    settings.DEBUG = True
 
 
 # Per: https://stackoverflow.com/a/78651148, this is fine to do iff for playwright
